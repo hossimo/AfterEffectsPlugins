da75de6a8f24ffc784f00fc7e1a555788c43ccfb
Anthony Headley <1986602+hossimo@users.noreply.github.com>
2022-12-20 16:18:14 -0500

    Added Outer Block

--- Zoom Out Selected Comps/Zoom Out Selected.jsx
+++ Zoom Out Selected Comps/Zoom Out Selected.jsx
@@ -17,17 +17,19 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var version = "1.0.0"
+{
+  var version = "1.0.0"
 
-// Get the selected compositions
-var selectedComps = app.project.selection;
+  // Get the selected compositions
+  var selectedComps = app.project.selection;
 
-for (var i = 0; i < selectedComps.length; i++) {
-  var comp = selectedComps[i];
-  if (comp.typeName != "Composition")
-    continue;
+  for (var i = 0; i < selectedComps.length; i++) {
+    var comp = selectedComps[i];
+    if (comp.typeName != "Composition")
+      continue;
 
-  // Set the work area start and end times to the beginning and end of the composition
-  comp.workAreaStart = 0;
-  comp.workAreaDuration = comp.duration;
+    // Set the work area start and end times to the beginning and end of the composition
+    comp.workAreaStart = 0;
+    comp.workAreaDuration = comp.duration;
+  }
 }
